32cf22ebc031e07aa5874b57919be6d078454a42
rtab-bcarr <brandon.carr@rusttaphouseandbar.com>
2023-11-30 18:47:45 -0500

    Add Vscode settings, finish sync script

--- scripts/sync.py
+++ scripts/sync.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 import shutil
+from pathlib import Path
 import os
+
+print('Starting')
 
 board_drive = 'D:/'
 board_ignore_paths = [
@@ -16,7 +19,7 @@ local_sync_dir = 'src/'
 
 def get_last_modified_time(file_path):
   last_m_time = os.path.getmtime(file_path)
-  return datetime.fromtimestamp(last_m_time)
+  return last_m_time
 
 def get_items_from_dir(dir_path):
   items = []
@@ -37,11 +40,33 @@ def get_items_from_dir(dir_path):
 local_dir = get_items_from_dir(local_sync_dir)
 board_dir = get_items_from_dir(board_drive)
 
-print('Local')
-print(local_dir)
-print('Board')
-print(board_dir)
+def find_in(item, arr):
+  for i in range(len(arr)):
+    arrItem = arr[i]['file']
+    if item['file'] == arrItem:
+      return arr[i]
+  return False
 
 # Remove All Files From Board that aren't in src
 for item in board_dir:
-  
+  # print(item)
+  should_remove = False
+  found_in_local = find_in(item, local_dir)
+  if found_in_local == False:
+    print(f'Removing: {item}...')
+    path = Path(item['path'])
+    path.unlink()
+
+# Update Files
+for local_file in local_dir:
+  found_file = find_in(local_file, board_dir)
+
+  if found_file == False:
+    # Create File
+    pass
+  else:
+    last_modified_on_board = get_last_modified_time(found_file['path'])
+    last_modified_on_local = get_last_modified_time(local_file['path'])
+    if last_modified_on_local > last_modified_on_board:
+      print(f'Updating {found_file["file"]}...')
+      shutil.copy(local_file['path'], found_file['path'])
